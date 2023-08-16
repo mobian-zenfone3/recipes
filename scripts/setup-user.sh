c8584bc73378c2d8677ca42fd9beb01f2c9f9817
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2023-08-16 15:06:49 +0200

    scripts: setup-user: drop unneeded groups
    Most of this is handled through logind granting access to the relevant
    devices when a session is created.
    
    Fixes #28

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -9,13 +9,8 @@ adduser --gecos "${USERNAME}" --disabled-password --shell /bin/bash "${USERNAME}
 adduser "${USERNAME}" sudo
 
 # Needed for hardware access rights
-adduser "${USERNAME}" video
-adduser "${USERNAME}" render
-adduser "${USERNAME}" audio
 adduser "${USERNAME}" bluetooth
 adduser "${USERNAME}" plugdev
-adduser "${USERNAME}" input
-adduser "${USERNAME}" dialout
 adduser "${USERNAME}" feedbackd
 
 echo "${USERNAME}:${PASSWORD}" | chpasswd
