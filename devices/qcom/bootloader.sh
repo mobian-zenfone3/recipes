1fa837cbb2c9b6a6c0b9081c4f7d7f6c5fb6a341
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2024-01-17 15:27:05 +0100

    devices: qcom: bootloader.sh: handle v2 bootimg format
    Some Android phones, such as the Fairphone 5, rely on a newer bootimg
    format, which embeds a separate copy of the DTB at a defined location.

--- devices/qcom/bootloader.sh
+++ devices/qcom/bootloader.sh
@@ -18,11 +18,15 @@ KERNEL_VERSION=$(linux-version list | tail -1)
 
 # Parse config for generic parameters for the current SoC
 SOC=$(tomlq -r "if .chipset then .chipset else \"${DEVICE}\" end" ${CONFIG})
+BOOTIMG_VERSION="$(tomlq -r 'if .bootimg.version then .bootimg.version else 0 end' ${CONFIG})"
 KERNEL_ADDR="$(tomlq -r '.bootimg.kernel + .bootimg.base' ${CONFIG})"
 RAMDISK_ADDR="$(tomlq -r '.bootimg.ramdisk + .bootimg.base' ${CONFIG})"
 SECOND_ADDR="$(tomlq -r '.bootimg.second + .bootimg.base' ${CONFIG})"
 TAGS_ADDR="$(tomlq -r '.bootimg.tags + .bootimg.base' ${CONFIG})"
 PAGE_SIZE="$(tomlq -r '.bootimg.pagesize' ${CONFIG})"
+if [ "${BOOTIMG_VERSION}" = "2" ]; then
+    DTB_ADDR="$(tomlq -r '.bootimg.dtb + .bootimg.base' ${CONFIG})"
+fi
 
 for i in $(seq 0 $(tomlq -r '.device | length - 1' ${CONFIG})); do
     # Parse device-specific parameters
@@ -50,7 +54,11 @@ for i in $(seq 0 $(tomlq -r '.device | length - 1' ${CONFIG})); do
         fi
     fi
 
-    # Append DTB to kernel
+    if [ "${BOOTIMG_VERSION}" = "2" ]; then
+        # v2 images also embed a separate copy of the DTB
+        EXTRA_ARGS="--header_version ${BOOTIMG_VERSION} --dtb_offset ${DTB_ADDR} --dtb ${DTB_FILE}"
+    fi
+
     echo "Creating boot image for ${FULLMODEL}..."
     cat /boot/vmlinuz-${KERNEL_VERSION} ${DTB_FILE} > /tmp/kernel-dtb
 
@@ -60,5 +68,5 @@ for i in $(seq 0 $(tomlq -r '.device | length - 1' ${CONFIG})); do
         --ramdisk_offset ${RAMDISK_ADDR} --ramdisk /boot/initrd.img-${KERNEL_VERSION} \
         --second_offset ${SECOND_ADDR} --tags_offset ${TAGS_ADDR} \
         --cmdline "mobile.root=${ROOTPART} ${CMDLINE} init=/sbin/init ro ${LOGLEVEL} splash" \
-        --pagesize ${PAGE_SIZE}
+        --pagesize ${PAGE_SIZE} ${EXTRA_ARGS}
 done
