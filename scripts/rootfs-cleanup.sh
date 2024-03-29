0d0f93ce3c7313e3c8e26fb57def6e9ac0e7f588
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2024-03-29 13:29:36 +0100

    scripts: rootfs-cleanup: call `apt-get` instead of `apt`
    `apt` assumes we're running in an interactive terminal, which can mess
    with the output when run in a script. `apt-get` doesn't exhibit the same
    issue, so we can use it instead.

--- scripts/rootfs-cleanup.sh
+++ scripts/rootfs-cleanup.sh
@@ -2,8 +2,8 @@
 
 # Remove apt packages which are no longer unnecessary and delete
 # downloaded packages
-apt -y autoremove --purge
-apt clean
+apt-get -y autoremove --purge
+apt-get clean
 
 # Remove machine ID so it gets generated on first boot
 rm -f /var/lib/dbus/machine-id
